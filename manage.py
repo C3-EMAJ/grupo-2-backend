3fe0f084d3bd3a968882fff9c7825c7b32a7bfca
Vitor De Ávila Freitas <vitordeavilafreitas@gmail.com>
2023-11-23 20:52:41 -0300

    Feat: Agora é possível adicionar o assistido junto ao representado. Também é possível deletar ambos.

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+    print(os.environ)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
