59de0b206dcbc5ebf2dd061857bb55657e7affe8
Vitor De Ávila Freitas <vitordeavilafreitas@gmail.com>
2023-11-25 22:44:39 -0300

    Fix: Ajustando o CORS na settings.py, para que ele tenha os headers corretos.

--- app/settings.py
+++ app/settings.py
@@ -76,12 +76,12 @@ CORS_ALLOW_HEADERS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'app.urls'
